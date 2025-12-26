ac01fb43ec48af49bcc164860c15cce96f4ab92f
mominali12 <mominali1296@gmail.com>
2025-12-26 17:46:45 +0100

    updates training script to use wandb logging

--- rail_road_scripts/training.py
+++ rail_road_scripts/training.py
@@ -2,11 +2,27 @@ from ultralytics import YOLO
 import os
 import wandb
 # from wandb.integration.ultralytics import add_wandb_callback
+from dotenv import load_dotenv
+import os
+from ultralytics.utils import SETTINGS
 
+# Load environment variables from .env
+load_dotenv()
+
+# Now you can use them
+wandb_api_key = os.getenv("WANDB_API_KEY")
+os.environ["WANDB_API_KEY"] = wandb_api_key  # for wandb
+print("WANDB_API_KEY loaded:", wandb_api_key)
 # Initialize your Weights & Biases environment
-wandb.login(key=os.getenv("WANDB_API_KEY"))
+wandb.login(key=wandb_api_key)
+SETTINGS["wandb"] = True
+
+
 # wandb.init(project="ultralytics", name="coco_8 forked_repo")
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+
+
 model = YOLO('/home/mal/coco_projects/ultralytics/yolov8/ultralytics/ultralytics/cfg/models/v8/yolov8.yaml')
 # model = YOLO("/home/mal/coco_projects/ultralytics/yolov8/ultralytics/train_scratch_coco_OS_Zol_BG_cargo_3004/weights/best.pt") 
 # model = YOLO('yolov8s.pt')
@@ -18,6 +34,6 @@ results = model.train(
     imgsz=640,
     patience=100,
     project="zollner project",
-    name="scratch_OS_OI_Zollner_train",
+    name="scratch_COCO_OS_OI_Zollner_train",
     )
 wandb.finish()
