bd3195cfa70b63bc0ead61ff184a72055915d51f
mominali12 <mominali1296@gmail.com>
2025-12-28 01:54:32 +0100

    updates script to continue training as *scratch_COCO_OS_OI_Zollner_train_cont*

--- rail_road_scripts/training.py
+++ rail_road_scripts/training.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 import os
 from ultralytics.utils import SETTINGS
 
+
 # Load environment variables from .env
 load_dotenv()
 
@@ -21,19 +22,19 @@ SETTINGS["wandb"] = True
 # wandb.init(project="ultralytics", name="coco_8 forked_repo")
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
-
-
-model = YOLO('/home/mal/coco_projects/ultralytics/yolov8/ultralytics/ultralytics/cfg/models/v8/yolov8.yaml')
+# model = YOLO('/home/mal/coco_projects/ultralytics/yolov8/ultralytics/ultralytics/cfg/models/v8/yolov8.yaml')
+model= YOLO("/home/mal/ultralytics/rail_road_scripts/zollner project/scratch_COCO_OS_OI_Zollner_train3/weights/last.pt")
 # model = YOLO("/home/mal/coco_projects/ultralytics/yolov8/ultralytics/train_scratch_coco_OS_Zol_BG_cargo_3004/weights/best.pt") 
 # model = YOLO('yolov8s.pt')
 # add_wandb_callback(model, enable_model_checkpointing=True)
 
 results = model.train(
     data='/home/mal/ultralytics/ultralytics/cfg/datasets/zollner_train.yaml',
-    epochs=100, 
+    epochs=1, 
     imgsz=640,
     patience=100,
     project="zollner project",
-    name="scratch_COCO_OS_OI_Zollner_train",
+    name="scratch_COCO_OS_OI_Zollner_train_cont",
     )
+
 wandb.finish()
