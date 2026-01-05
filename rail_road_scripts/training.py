4a7728c7132ef14cbfd1486b7f3a28efceac5505
mominali12 <mominali1296@gmail.com>
2026-01-05 23:48:56 +0100

    changes training script for single class experiment

--- rail_road_scripts/training.py
+++ rail_road_scripts/training.py
@@ -22,19 +22,21 @@ SETTINGS["wandb"] = True
 # wandb.init(project="ultralytics", name="coco_8 forked_repo")
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
-# model = YOLO('/home/mal/coco_projects/ultralytics/yolov8/ultralytics/ultralytics/cfg/models/v8/yolov8.yaml')
-model= YOLO("/home/mal/ultralytics/rail_road_scripts/zollner project/scratch_COCO_OS_OI_Zollner_train3/weights/last.pt")
+model = YOLO('/home/mal/coco_projects/ultralytics/yolov8/ultralytics/ultralytics/cfg/models/v8/yolov8.yaml')
+model.model.args["scale"] = "s"
+# model= YOLO("/home/mal/ultralytics/rail_road_scripts/zollner project/scratch_COCO_OS_OI_Zollner_train3/weights/last.pt")
 # model = YOLO("/home/mal/coco_projects/ultralytics/yolov8/ultralytics/train_scratch_coco_OS_Zol_BG_cargo_3004/weights/best.pt") 
 # model = YOLO('yolov8s.pt')
 # add_wandb_callback(model, enable_model_checkpointing=True)
 
 results = model.train(
-    data='/home/mal/ultralytics/ultralytics/cfg/datasets/zollner_train.yaml',
-    epochs=1, 
+    data='/home/mal/ultralytics/ultralytics/cfg/datasets/zollner_train_single_cls.yaml',
+    epochs=300, 
     imgsz=640,
     patience=100,
     project="zollner project",
-    name="scratch_COCO_OS_OI_Zollner_train_cont",
+    single_cls=False,
+    name="single_class_train",
     )
 
 wandb.finish()
