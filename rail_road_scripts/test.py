686205f600aa77134519b4530b5439326bcd4bdc
mominali12 <mominali1296@gmail.com>
2026-01-19 00:58:38 +0100

    ran coco_zollner_single_cls_test

--- rail_road_scripts/test.py
+++ rail_road_scripts/test.py
@@ -16,7 +16,7 @@ print("WANDB_API_KEY loaded:", wandb_api_key)
 #Initialize your Weights & Biases environment
 wandb.login(key=wandb_api_key)
 SETTINGS["wandb"] = True
-run_name = "pre-trained_yolov8s_zollner_test"
+run_name = "coco_zollner_single_cls_test"
 
 run = wandb.init(
     project="zollner project",
@@ -27,7 +27,7 @@ run.tags = run.name.split("_")
 
 # wandb.init(project="ultralytics", name="coco_8 forked_repo")
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-model = YOLO('yolov8s.pt')
+model = YOLO('/home/mal/ultralytics/rail_road_scripts/BEST_coco_zollner_single_cls_train5.pt')
 
 #UPDATE HERE
 # data_yaml = '/home/mal/coco_projects/ultralytics/yolov8/ultralytics/ultralytics/cfg/datasets/zollner_train_single_cls.yaml'
